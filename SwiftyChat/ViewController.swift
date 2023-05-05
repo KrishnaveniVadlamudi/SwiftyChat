9c6a354959f8ec73903bc7d12bc9eb11fc3f7211
KrishnaveniVadlamudi <veni455@gmail.com>
2023-05-05 09:42:38 -0700

    First comitt

---
+++ SwiftyChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyChat
+//
+//  Created by Sunkara on 5/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
